06db0c18b761404a26d562c5b4eb533ab70668a7
I-Huan CHIU <ichiu@chem.sci.osaka-u.ac.jp>
2021-03-08 16:29:38 +0900

    update spectrum and 2d image script for MC

--- macro/spec.py
+++ macro/spec.py
@@ -22,7 +22,7 @@ __location__ = os.path.realpath(
 ROOT.gROOT.LoadMacro( __location__+'/AtlasStyle/AtlasStyle.C')
 ROOT.SetAtlasStyle()
 
-def createRatioCanvas(Name = "cs", w = 1200, h = 800):
+def createRatioCanvas(Name = "cs", w = 1000, h = 800):
     cRatioCanvas = ROOT.TCanvas(Name,"",0,0,int(w),int(h))
     cRatioCanvas.GetFrame().SetBorderMode(0)
     cRatioCanvas.GetFrame().SetBorderSize(0)
@@ -37,40 +37,37 @@ def createRatioCanvas(Name = "cs", w = 1200, h = 800):
 def plot(args):
     f = ROOT.TFile(args.input)   
     tree = f.Get("tree")
-    nbin=32
-    if tree.GetEntries() > 50000: nbin=128
-    elif tree.GetEntries() > 10000: nbin=64
-    else:nbin=32
 
-    ecut_base=TCut("Hit_y < {}".format(-180))
+    cv  = createRatioCanvas("cv", 1000, 800)
+
     ecut_s = TCut("Stop_VolumeID == {}".format(3))#sample
     ecut_b = TCut("Stop_VolumeID == {}".format(4))#Al
+    tree.Draw("Hit_Energy_Reso*1000 >> h_a(600,0,150)","","")
+    tree.Draw("Hit_Energy_Reso*1000 >> h_s(600,0,150)",ecut_s,"")
+    tree.Draw("Hit_Energy_Reso*1000 >> h_b(600,0,150)",ecut_b,"")
+    h_a, h_s, h_b=gDirectory.Get("h_a"), gDirectory.Get("h_s"), gDirectory.Get("h_b")
+    h_a.GetXaxis().SetTitle("Energy[kev]")
+    h_a.GetYaxis().SetTitle("Counts (/0.25keV)")
+    h_a.SetLineColor(1)
+    h_s.SetLineColor(2)
+    h_b.SetLineColor(4)
+    h_a.Draw()
+    h_b.Draw("same")
+    h_s.Draw("same")
+    leg=ROOT.TLegend(.60,.70,.80,.85)
+    leg.AddEntry(h_a,  "All", "l");
+    leg.AddEntry(h_s,  "Signal", "l");
+    leg.AddEntry(h_b,   "Bkg.",   "l");
+    leg.Draw("same");
    
-    tree.Draw("Hit_z:Hit_x >> h_s({0},-16,16,{1},-16,16)".format(nbin,nbin),ecut_base+ecut_s,"")
-    tree.Draw("Hit_z:Hit_x >> h_b({0},-16,16,{1},-16,16)".format(nbin,nbin),ecut_base+ecut_b,"")
-    h_s,h_b=gDirectory.Get("h_s"),gDirectory.Get("h_b")
-    h_s.GetXaxis().SetTitle("X[mm]")
-    h_s.GetYaxis().SetTitle("Y[mm]")
-    h_b.GetXaxis().SetTitle("X[mm]")
-    h_b.GetYaxis().SetTitle("Y[mm]")
-
-    cvs  = createRatioCanvas("cvs", 1200, 1000)
-    SetMyPalette("AD")
-    h_s.Draw("colz")
     name=args.input 
-    name=name.replace(".root","_2d_s.pdf")
-    name=name.replace("root/Output_","figs/Image_")   
-    cvs.Print(name)
+    name=name.replace(".root","_energy.pdf")
+    name=name.replace("root/Output_","figs/Spec_")   
+    cv.Print(name)
 
-    cvb  = createRatioCanvas("cvb", 1200, 1000)
-    SetMyPalette("AD")
-    h_b.Draw("colz")
-    name=name.replace("_2d_s.pdf","_2d_b.pdf")
-    cvb.Print(name)
-    
 if __name__ == '__main__' : 
   parser = argparse.ArgumentParser(description='Process some integers.')
-  parser.add_argument("input", type=str, default="./20151112_00009_001.root", help="Input File Name")
+  parser.add_argument("input", type=str, default="./root/20151112_00009_001.root", help="Input File Name")
   args = parser.parse_args()
 
   plot( args )
